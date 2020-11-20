b744039ede12c91dec50ff7a82764037bbca4fa3
Henrique Pappis <contact@henriquepappis.com>
2020-11-20 15:59:24 -0300

    close modal added

--- src/Application/views/activity/remove.php
+++ src/Application/views/activity/remove.php
@@ -5,5 +5,14 @@
     <input type="hidden" name="id" value="<?php echo $activity['id']; ?>">
     <h2>Você tem certeza que deseja excluir a tarefa ? </h2>
     <button name="submit" type="submit">Sim</button>
-    <button type="reset">Cancelar</button>
+    <button type="button" class="close" data-dismiss="modal" aria-label="Close">Cancelar</button>
 </form>
+
+<script>
+    $(document).ready(function() {
+        $( ".close" ).click(function() {
+            $(this).modal('hide');
+            window.history.back();
+        });
+    });
+</script>
